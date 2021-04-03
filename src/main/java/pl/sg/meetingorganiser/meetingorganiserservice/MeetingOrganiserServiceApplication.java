b54c8b6c9509f44b2822dbd799a1847fddda9196
Aleksander Kołata <olek.tbg.konta@gmail.com>
2021-04-03 20:36:08 +0200

    [MO-003] initial project configured

---
+++ src/main/java/pl/sg/meetingorganiser/meetingorganiserservice/MeetingOrganiserServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.sg.meetingorganiser.meetingorganiserservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingOrganiserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingOrganiserServiceApplication.class, args);
+    }
+}
